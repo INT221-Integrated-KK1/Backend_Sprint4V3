80617f7bce792cdbffc9b88adb2824d49110a1de
findmelily <pinny.pinngam@gmail.com>
2024-11-25 20:17:09 +0700

    fix invite response

--- src/main/java/com/example/int221integratedkk1_backend/DTOS/InviteCollaboratorResponse.java
+++ src/main/java/com/example/int221integratedkk1_backend/DTOS/InviteCollaboratorResponse.java
@@ -1,12 +1,10 @@
 package com.example.int221integratedkk1_backend.DTOS;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
